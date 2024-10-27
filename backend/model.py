ed9ee1053d1fabb45cbac5e4fc809c1813df0da9
Yash Gandhi <43007052+YashGandhi17@users.noreply.github.com>
2024-10-26 18:44:23 -0700

    Add files via upload

--- backend/model.py
+++ backend/model.py
@@ -1,24 +1,70 @@
 import os
 import config
+from pydantic import BaseModel, Field
+from pydantic import BaseModel, confloat
+from langchain_core.prompts import ChatPromptTemplate
+
+class MyModel(BaseModel):
+    correctness_score: confloat(ge=0.0, le=1.0)
+    suggestion: str = Field(description="2 lines suggestion to student to score better.")
+
+from langchain_groq import ChatGroq
+
+llm = ChatGroq(
+    model="mixtral-8x7b-32768",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    api_key=config.api_key
+    # other params...
+)
+structured_llm = llm.with_structured_output(MyModel)
 
 from groq import Groq
+import os
 
-groq_client = Groq(
-    api_key= config.api_key,
-)
+if "GROQ_API_KEY" not in os.environ:
+    os.environ["GROQ_API_KEY"] = config.api_key
+
+# groq_client = Groq(
+#     api_key= config.api_key,
+# )
+
+# def getModelResponse(memory_prompt,user_prompt):
+
+#     chat_completion = groq_client.chat.completions.create(
+#         messages=[
+#             {
+#                 "role": "system",
+#                 "content": memory_prompt,
+#                 "role": "user",
+#                 "content" : user_prompt,
+#             }
+#         ],
+#         model="llama3-8b-8192"
+#     )
+
+#     return chat_completion.choices[0].message.content
 
 def getModelResponse(memory_prompt,user_prompt):
 
-    chat_completion = groq_client.chat.completions.create(
-        messages=[
-            {
-                "role": "system",
-                "content": memory_prompt,
-                "role": "user",
-                "content" : user_prompt,
-            }
-        ],
-        model="llama3-8b-8192"
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "{system}",
+            ),
+            ("human", "{user}"),
+        ]
     )
 
-    return chat_completion.choices[0].message.content
+    chain = prompt | structured_llm
+    output = chain.invoke(
+        {
+            "system": memory_prompt,
+            "user" : user_prompt
+
+    }
+    )
+    return output
